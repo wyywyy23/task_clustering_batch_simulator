6e4b451ad1ef8e7830df247dd430304c9dd3a769
henricasanova <henric@hawaii.edu>
2019-04-12 18:11:41 -1000

    bug--

--- src/StaticClusteringAlgorithms/StaticClusteringWMS.cpp
+++ src/StaticClusteringAlgorithms/StaticClusteringWMS.cpp
@@ -101,7 +101,7 @@ std::set<ClusteredJob *> StaticClusteringWMS::createClusteredJobs() {
             throw std::invalid_argument("Invalid static:one_job-m specification");
         }
 
-        unsigned long waste_bound = std::stod(tokens[2]);
+        double waste_bound = std::stod(tokens[2]);
 
         ClusteredJob *job = new ClusteredJob();
         for (auto t : this->getWorkflow()->getTasks()) {
@@ -320,6 +320,7 @@ void StaticClusteringWMS::submitClusteredJob(ClusteredJob *clustered_job) {
     }
 
     double makespan = WorkflowUtil::estimateMakespan(clustered_job->getTasks(), num_nodes, this->core_speed);
+    std::cout << "MAKESPAN ESTIMATE = " << makespan << "\n";
 
     std::map<std::string, std::string> batch_job_args;
     batch_job_args["-N"] = std::to_string(num_nodes);
@@ -335,6 +336,8 @@ void StaticClusteringWMS::submitClusteredJob(ClusteredJob *clustered_job) {
 
     try {
         WRENCH_INFO("Submitting a batch job...");
+        std::cout << "REQUESTING " << (unsigned long) (1 + (makespan * EXECUTION_TIME_FUDGE_FACTOR)) << " " << num_nodes << "\n";
+
         this->job_manager->submitJob(standard_job, batch_service, batch_job_args);
 //    this->job_map.insert(std::make_pair(standard_job, clustered_job));
     } catch (WorkflowExecutionException &e) {
--- src/TestClusteringAlgorithm/TestPlaceHolderJob.h
+++ src/TestClusteringAlgorithm/TestPlaceHolderJob.h
@@ -14,17 +14,21 @@ namespace wrench {
 
     public:
         TestPlaceHolderJob(PilotJob *pilot_job,
+            unsigned long num_nodes,
             std::vector<WorkflowTask *> tasks,
             unsigned long start_level,
             unsigned long end_level) : pilot_job(pilot_job),
+            num_nodes(num_nodes),
             tasks(tasks),
             start_level(start_level),
             end_level(end_level) { }
 
             PilotJob *pilot_job;
+            unsigned long num_nodes;
             std::vector<WorkflowTask *> tasks;
             unsigned long start_level;
             unsigned long end_level;
+            unsigned long num_currently_running_tasks = 0;
         };
 
     };
