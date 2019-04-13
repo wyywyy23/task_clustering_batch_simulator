73fb7af58fd6b73b353d67a85666ac9cb4ceff81
henricasanova <henric@hawaii.edu>
2019-04-12 18:44:42 -1000

    Removed scheduling noise

--- src/TestClusteringAlgorithm/TestPlaceHolderJob.h
+++ src/TestClusteringAlgorithm/TestPlaceHolderJob.h
@@ -14,23 +14,36 @@ namespace wrench {
 
     public:
         TestPlaceHolderJob(PilotJob *pilot_job,
-            unsigned long num_nodes,
-            std::vector<WorkflowTask *> tasks,
-            unsigned long start_level,
-            unsigned long end_level) : pilot_job(pilot_job),
-            num_nodes(num_nodes),
-            tasks(tasks),
-            start_level(start_level),
-            end_level(end_level) { }
+                           unsigned long num_nodes,
+                           std::vector<WorkflowTask *> tasks,
+                           unsigned long start_level,
+                           unsigned long end_level) : pilot_job(pilot_job),
+                                                      num_nodes(num_nodes),
+                                                      start_level(start_level),
+                                                      end_level(end_level) {
 
-            PilotJob *pilot_job;
-            unsigned long num_nodes;
-            std::vector<WorkflowTask *> tasks;
-            unsigned long start_level;
-            unsigned long end_level;
-            unsigned long num_currently_running_tasks = 0;
-        };
+            this->tasks = tasks;
+            // Sort the tasks by decreasing flops!
+            std::sort(this->tasks.begin(), this->tasks.end(),
+                      [](const WorkflowTask*  t1, const WorkflowTask*  t2) -> bool
+                      {
+                          if (t1->getFlops() == t2->getFlops()) {
+                              return ((uintptr_t) t1 > (uintptr_t) t2);
+                          }
+                          return (t1->getFlops() >= t2->getFlops());
 
+                      });
+
+        }
+
+        PilotJob *pilot_job;
+        unsigned long num_nodes;
+        std::vector<WorkflowTask *> tasks;
+        unsigned long start_level;
+        unsigned long end_level;
+        unsigned long num_currently_running_tasks = 0;
     };
 
-    #endif //YOUR_PROJECT_NAME_EVANPLACEHOLDERJOB_H
+};
+
+#endif //YOUR_PROJECT_NAME_EVANPLACEHOLDERJOB_H
