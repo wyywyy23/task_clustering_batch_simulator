b0c6fcc2ae255f1577423b067aaaf167fd0914af
Evan Hataishi <evanhata@hawaii.edu>
2019-08-24 22:43:38 -1000

    Create generic placeholder job file

---
+++ src/PlaceHolderJob.h
@@ -0,0 +1,32 @@
+
+
+#ifndef YOUR_PROJECT_NAME_PLACEHOLDERJOB_H
+#define YOUR_PROJECT_NAME_PLACEHOLDERJOB_H
+
+#include <vector>
+
+namespace wrench {
+
+    class WorkflowTask;
+    class PilotJob;
+
+    class PlaceHolderJob {
+
+    public:
+        PlaceHolderJob(PilotJob *pilot_job,
+                       std::vector<WorkflowTask *> tasks,
+                       unsigned long start_level,
+                       unsigned long end_level) : pilot_job(pilot_job),
+                                                  tasks(tasks),
+                                                  start_level(start_level),
+                                                  end_level(end_level) { }
+
+        PilotJob *pilot_job;
+        std::vector<WorkflowTask *> tasks;
+        unsigned long start_level;
+        unsigned long end_level;
+    };
+
+};
+
+#endif //YOUR_PROJECT_NAME_PLACEHOLDERJOB_H
