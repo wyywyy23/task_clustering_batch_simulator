ce2a4de9c20f86bdcf22f12676d9b5b7b9e37502
Evan Hataishi <evanhata@hawaii.edu>
2019-08-29 00:41:00 -1000

    Refactor reused code to this proxywms

--- src/Util/ProxyWMS.cpp
+++ src/Util/ProxyWMS.cpp
@@ -8,7 +8,7 @@
 #include "ProxyWMS.h"
 #include "PlaceHolderJob.h"
 
-XBT_LOG_NEW_DEFAULT_CATEGORY(proxy_wms,"Log category for Proxy WMS");
+XBT_LOG_NEW_DEFAULT_CATEGORY(proxy_wms, "Log category for Proxy WMS");
 
 
 namespace wrench {
@@ -28,9 +28,9 @@ namespace wrench {
         requested_execution_time = requested_execution_time * EXECUTION_TIME_FUDGE_FACTOR;
 
         // Aggregate tasks
-        std::vector < WorkflowTask * > tasks;
+        std::vector<WorkflowTask *> tasks;
         for (unsigned long l = start_level; l <= end_level; l++) {
-            std::vector < WorkflowTask * > tasks_in_level = this->workflow->getTasksInTopLevelRange(l, l);
+            std::vector<WorkflowTask *> tasks_in_level = this->workflow->getTasksInTopLevelRange(l, l);
             for (auto t : tasks_in_level) {
                 if (t->getState() != WorkflowTask::COMPLETED) {
                     tasks.push_back(t);
@@ -39,28 +39,69 @@ namespace wrench {
         }
 
         // Submit the pilot job
-        std::map <std::string, std::string> service_specific_args;
+        std::map<std::string, std::string> service_specific_args;
         service_specific_args["-N"] = std::to_string(requested_parallelism);
         service_specific_args["-c"] = "1";
         service_specific_args["-t"] = std::to_string(1 + ((unsigned long) requested_execution_time) / 60);
 
-        PlaceHolderJob *pj = new PlaceHolderJob(this->job_manager->createPilotJob(), tasks, start_level, end_level,
-                                                requested_execution_time);
+        PlaceHolderJob *pj = new PlaceHolderJob(this->job_manager->createPilotJob(), tasks, start_level, end_level);
 
-        /**
         WRENCH_INFO("Submitting a Pilot Job (%ld hosts, %.2lf sec) for workflow levels %ld-%ld (%s)",
                     requested_parallelism, requested_execution_time, start_level, end_level,
                     pj->pilot_job->getName().c_str());
 
         WRENCH_INFO("This pilot job has these tasks:");
-        for (auto t : pj->tasks) {
-            WRENCH_INFO("     - %s", t->getID().c_str());
+        for (auto t : pj->tasks) { WRENCH_INFO("     - %s", t->getID().c_str());
         }
-        */
 
         this->job_manager->submitJob(pj->pilot_job, this->batch_service, service_specific_args);
 
         return pj;
     }
 
+    void ProxyWMS::submitAllOneJobPerTask(double core_speed) {
+        for (auto task : this->workflow->getTasks()) {
+            if (task->getState() == WorkflowTask::State::READY) {
+                StandardJob *standard_job = this->job_manager->createStandardJob(task, {});
+                std::map<std::string, std::string> service_specific_args;
+                // TODO - this cast is horrible, but should be okay?
+                unsigned long requested_execution_time =
+                        (unsigned long) (task->getFlops() / core_speed) * EXECUTION_TIME_FUDGE_FACTOR;
+                service_specific_args["-N"] = "1";
+                service_specific_args["-c"] = "1";
+                service_specific_args["-t"] = std::to_string(1 + ((unsigned long) requested_execution_time) / 60);
+
+                WRENCH_INFO("Submitting task %s individually!", task->getID().c_str());
+
+                this->job_manager->submitJob(standard_job, this->batch_service, service_specific_args);
+            }
+        }
+    }
+
+    double ProxyWMS::findMaxDuration(std::set<wrench::PlaceHolderJob *> jobs) {
+        double max_duration = 0;
+        for (PlaceHolderJob *pj : jobs) {
+            max_duration = std::max<double>(max_duration, pj->getDuration());
+        }
+
+        return max_duration;
+    }
+
+    // TODO check if this works then replace
+    double ProxyWMS::estimateWaitTime(long parallelism, double makespan, double simulation_date, int *sequence) {
+        std::set<std::tuple<std::string, unsigned int, unsigned int, double>> job_config;
+        std::string config_key = "config_XXXX_" + std::to_string((*sequence)++); // need to make it unique for BATSCHED
+        job_config.insert(std::make_tuple(config_key, (unsigned int) parallelism, 1, makespan));
+        std::map<std::string, double> estimates = this->batch_service->getStartTimeEstimates(job_config);
+
+        if (estimates[config_key] < 0) {
+            throw std::runtime_error("Could not obtain start time estimate... aborting");
+        }
+
+        double wait_time_estimate = std::max<double>(0, estimates[config_key] - simulation_date);
+
+        return wait_time_estimate;
+    }
+
+    // TODO - implement getStartLevel() in here??
 }
