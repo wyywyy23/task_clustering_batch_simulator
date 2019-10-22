54638de94b4d26461e907f1e9810be16b1b408d2
Evan Hataishi <evanhata@hawaii.edu>
2019-10-21 17:37:33 -1000

    Add and extra test

--- docker/test-run.sh
+++ docker/test-run.sh
@@ -7,3 +7,5 @@ sudo docker run -v $PWD:/output wrenchproject/task-clustering:latest 100 /simula
 sudo docker run -v $PWD:/output wrenchproject/task-clustering:latest 100 /simulator/trace_files/kth_sp2.json 10 dax:/simulator/workflows/CYBERSHAKE_50_360000.dax 86400 zhang:noglobal:bsearch:prediction conservative_bf --wrench-no-log /output/cshake.json
 
 sudo docker run -v $PWD:/output wrenchproject/task-clustering:latest 100 /simulator/trace_files/kth_sp2.json 10 dax:/simulator/workflows/GENOME_50_360000.dax 86400 zhang:noglobal:bsearch:prediction conservative_bf --wrench-no-log /output/genome.json
+
+sudo docker run -v $PWD:/output wrenchproject/task-clustering:latest 100 /simulator/trace_files/kth_sp2.json 100 dax:/simulator/workflows/CYBERSHAKE_250_3600000.dax 104400 zhang:noglobal:nobsearch:prediction conservative_bf --wrench-no-log /output/test.json
