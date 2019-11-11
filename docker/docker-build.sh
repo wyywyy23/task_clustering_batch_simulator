dc43cb8a38a603cf424571d2c79da60ecf8ab258
Rafael Ferreira da Silva <rafsilva@isi.edu>
2019-11-11 14:26:38 -0800

    adding data files

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -4,8 +4,8 @@ echo "Building..."
 mkdir -p build/workflows
 mkdir -p build/trace_files
 # Swap out path to where these files on your system
-cp -r ../../workflow-generator/workflows/* build/workflows/
-cp -r ../../batch_logs/swf_traces_json/* build/trace_files
+cp -r data/workflows/* build/workflows/
+cp -r data/trace_files/* build/trace_files
 # cp config.json build/
 docker build -t wrenchproject/task-clustering:latest .
 rm -rf build
